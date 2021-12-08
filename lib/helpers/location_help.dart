bdd1663679ee9d7e848b4f076b1a9c4f9af12b89
muhammedrishadpe <rishadpe@gmail.com>
2021-12-08 21:08:31 +0530

    Displaying a static map snapshot

---
+++ lib/helpers/location_help.dart
@@ -0,0 +1,10 @@
+const GOOGLE_API_KEY = 'AIzaSyCwZEmPvE83iHeXixOKa-66-v3LEDQp1a0';
+
+class LocationHelper {
+  static String generateLocationPreviewImage({
+    double latitude,
+    double longitude,
+  }) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+}
